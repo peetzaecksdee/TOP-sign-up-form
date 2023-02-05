79044ff41c28896cb53d24b996f204946c7e5a03
peetzaecksdee <peetzaecksdee@hotmail.com>
2023-02-05 18:08:46 +0700

    Finished the password confirmation

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(123);
+const passwordBox = document.getElementById("pass");
+const confirmPasswordBox = document.getElementById("cpass");
+const errorText = document.querySelector(".error");
+const submitBtn = document.getElementById("submit-btn");
+
+passwordBox.addEventListener("keyup", () => checkPass());
+confirmPasswordBox.addEventListener("keyup", () => checkPass());
+
+function checkPass() {
+	if (passwordBox.value != confirmPasswordBox.value) {
+		errorText.textContent = "* Password do not match";
+    submitBtn.disabled = true
+	} else {
+    errorText.textContent = ""
+    submitBtn.disabled = false
+  }
+}
+checkPass()
